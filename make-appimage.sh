d75a6600e31bbc5397d424fcc274372f7a8bfea0
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:00:11 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -19,7 +19,3 @@ quick-sharun /usr/bin/kmahjongg /usr/share/kmahjongg /usr/share/config.kcfg /usr
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
